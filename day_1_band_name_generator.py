4c3b19b837bff9753ae1bb4bf7fba7ba74ff5991
Ryan Bernardo <ryanbernardo212@gmail.com>
2022-02-01 22:04:49 +0100

    added comments

--- day_1_band_name_generator.py
+++ day_1_band_name_generator.py
@@ -1,4 +1,7 @@
+# Prints a welcome text to the screen.
 print("Welcome to the Band Name Generator!")
+# Asks for user inputs and then assign them to variables.
 city = input('What is the name of the town you grew up in? \n')
 pet input('What is the name of your first pet? \n')
+# Concatenates the data and prints it with a text.
 print('Your band name could be: ' + city + ' ' + pet)
