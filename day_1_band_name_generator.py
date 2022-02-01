7c8ea23524c08a382870cb076c7fb0666289d2a7
Ryan Bernardo <ryanbernardo212@gmail.com>
2022-02-01 21:56:54 +0100

    intial commit

---
+++ day_1_band_name_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator!")
+city = input('What is the name of the town you grew up in? \n')
+pet input('What is the name of your first pet? \n')
+print('Your band name could be: ' + city + ' ' + pet)
